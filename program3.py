94b9bce7308590540dad69e4bfd5681082dc45a7
Maciejo231 <maciek8honda@gmail.com>
2023-06-19 20:53:09 +0200

    dodano program

---
+++ program3.py
@@ -0,0 +1,9 @@
+# Pobieranie dwóch liczb od użytkownika
+liczba1 = float(input("Podaj pierwszą liczbę: "))
+liczba2 = float(input("Podaj drugą liczbę: "))
+
+# Obliczanie sumy
+suma = liczba1 + liczba2
+
+# Wyświetlanie wyniku
+print("Suma:", suma)
